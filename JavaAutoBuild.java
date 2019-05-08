bd9813672cb4bf2fcb2f82e00f40aa3bbeff6784
Jotasanhueza <jorge.sanhueza.b@gmail.com>
2019-05-08 14:48:26 -0400

    Inicial

---
+++ JavaAutoBuild.java
@@ -0,0 +1,7 @@
+public class JavaAutoBuild {
+
+  public static void main(String[] args) {
+    System.out.println("Hola Docker Workshop!");
+    System.out.println("Este es el resultado de AutoBuild!");
+  }
+}
