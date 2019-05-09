519fac1c05f679b20f984ca732d3c64342bcbd7a
jsanhuezaiia <50465192+jsanhuezaiia@users.noreply.github.com>
2019-05-09 17:08:25 -0400

    Detele

--- JavaAutoBuild.java
+++
@@ -1,7 +0,0 @@
-public class JavaAutoBuild {
-
-  public static void main(String[] args) {
-    System.out.println("Hola Docker Workshop!");
-    System.out.println("Este es el resultado de AutoBuild!");
-  }
-}
